07747399b8dd2ad023383e239af004e65a57b5b1
yeaung <yeaung@DESKTOP-4MELPK3.ya>
2021-04-23 15:42:24 +0630

    env variable change in database.py

--- database.py
+++ database.py
@@ -10,12 +10,8 @@ def cache_dotenv():
     load_dotenv()
 cache_dotenv()
 
-DATABASE_URL = os.environ.get('DATABASE_URL') 
-DATABASE_USERNAME = os.environ.get('DATABASE_USERNAME')
-DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
-DATABASE_NAME = os.environ.get('DATABASE_NAME')
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}/{DATABASE_NAME}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
